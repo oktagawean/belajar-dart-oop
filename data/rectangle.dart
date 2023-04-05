af594deeca3216063a5f44c2d8aa6cab03c14616
oktagawean <okta.gawean@gmail.com>
2023-04-05 11:10:12 +0700

    membuat getter_setter tanpa validasi

---
+++ data/rectangle.dart
@@ -0,0 +1,10 @@
+class Rectangle {
+  int _width = 1;
+  int _length = 1;
+
+  int get width => _width;
+  set width(int value) => _width = value;
+
+  int get length => _length;
+  set length(int value) => _length = value;
+}
---
+++ setter_getter.dart
@@ -0,0 +1,10 @@
+import 'data/rectangle.dart';
+
+void main() {
+  var rectangle = Rectangle();
+  rectangle.width = 100;
+  rectangle.length = 100;
+
+  print(rectangle.width);
+  print(rectangle.length);
+}
